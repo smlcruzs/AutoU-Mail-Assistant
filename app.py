1766cee16fccd90c1fb8dfde63ffb10a576f8f06
Samuel Cruz dos Santos <56977862+smlcruzs@users.noreply.github.com>
2026-03-15 01:57:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ def health():
     return jsonify({"ok": True})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8000")), debug=True)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "8080")), debug=True)
